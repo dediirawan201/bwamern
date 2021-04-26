f3ddd9def8e99e9d2321e737d45b80822dbd7173
Dedi Irawan <dediirwansyah1925@gmail.com>
2021-04-26 08:56:23 +0700

    create header component and pput to arrange LandingPage

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import Header from "parts/Header"
+import React from "react";
+
+const LandingPage = (props) => {
+    return (
+      <div>
+        <Header {...props}></Header>
+      </div>
+    );
+  }
+
+
+export default LandingPage;
